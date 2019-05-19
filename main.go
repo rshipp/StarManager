eb8476fd6998263afb566b59b44233a30757b7ad
rshipp <github@rshipp.com>
2019-05-19 17:02:09 -0500

    Add DB connection, App, and Model

--- main.go
+++ main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
-	"net/http"
+  "net/http"
+  "github.com/jinzhu/gorm"
+  _ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
-	w.Write([]byte("hello world"))
+type Star struct {
+  Name string `gorm:"primary_key"`
+  Description string
+  URL string
+}
+
+type App struct {
+  DB *gorm.DB
+}
+
+func (a *App) Initialize(dbDriver string, dbURI string) {
+  db, err := gorm.Open(dbDriver, dbURI)
+  if err != nil {
+    panic("failed to connect database")
+  }
+  a.DB = db
+
+  // Migrate the schema
+  a.DB.AutoMigrate(&Star{})
+}
+
+func (a *App) indexHandler(w http.ResponseWriter, r *http.Request) {
+  // Create a test Star.
+  a.DB.Create(&Star{Name: "test"})
+
+  // Read from DB.
+  var star Star
+  a.DB.First(&star, "name = ?", "test")
+
+  // Write to HTTP response.
+  w.WriteHeader(200)
+  w.Write([]byte(star.Name))
+
+  // Delete.
+  a.DB.Delete(&star)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
-	}
+  a := &App{}
+  a.Initialize("sqlite3", "test.db")
+
+  http.HandleFunc("/", a.indexHandler)
+  if err := http.ListenAndServe(":8080", nil); err != nil {
+    panic(err)
+  }
+
+  defer a.DB.Close()
 }
