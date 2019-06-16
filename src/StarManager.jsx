bd09aaa8d4261cf2a043696bff2d1660b9223692
rshipp <github@rshipp.com>
2019-06-15 19:20:15 -0500

    Add ajax and basic output to StarManager component

--- src/StarManager.jsx
+++ src/StarManager.jsx
@@ -1,10 +1,59 @@
 'use strict';
 
 class StarManager extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/stars")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log('here');
+          console.log(result);
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    return (
-      <h1>StarManager</h1>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <h1>StarManager</h1>
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                <a href={item.url}>{item.name}</a> {item.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
