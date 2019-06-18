8a012871159139383caad5606df62a6437a577f2
rshipp <github@rshipp.com>
2019-06-18 18:49:53 -0500

    Add react-axe back in

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// Use aXe a11y testing in development.
+var axe = require('react-axe');
+
+if (process.env.NODE_ENV !== 'production') {
+  axe(React, ReactDOM, 1000);
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
