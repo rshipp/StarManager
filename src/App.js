7ee9e14343e9a9dadf65f409492f983f7b3785ad
rshipp <github@rshipp.com>
2019-06-18 18:41:32 -0500

    Rebuild everything with create-react-app

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/stars")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <h1>StarManager</h1>
+          <ul>
+            {items.map(item => (
+              <li key={item.id}>
+                <a href={item.url}>{item.name}</a> {item.description}
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
